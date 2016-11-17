302ae5696a15dec11adb6dec3e3c0d8dcf999775
kbirk <birk.kevin@gmail.com>
2016-11-17 13:27:42 -0500

    Drop goji dependency.

--- middleware/log.go
+++ middleware/log.go
@@ -2,17 +2,26 @@ package middleware
 
 import (
 	"bytes"
+	"fmt"
 	"net/http"
+	"os"
+	"runtime"
 	"strings"
 	"time"
 
+	"github.com/mattn/go-isatty"
+	"github.com/mgutz/ansi"
 	"github.com/unchartedsoftware/plog"
 	"github.com/vova616/xxhash"
-	"github.com/zenazn/goji/web/mutil"
 )
 
-// Log is a middleware that logs each request. When standard output is a
-// TTY, Log will print in color, otherwise it will print in black and white.
+var (
+	colorTTY = isatty.IsTerminal(os.Stdout.Fd()) && (runtime.GOOS != "windows")
+)
+
+// Log is a middleware that logs each request. Inspired from the logging
+// middleware from zenazn/goji:
+// https://github.com/zenazn/goji/blob/master/web/middleware/logger.go
 func Log(h http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
 		if isWebsocketUpgrade(r) {
@@ -22,64 +31,79 @@ func Log(h http.Handler) http.Handler {
 			h.ServeHTTP(w, r)
 			return
 		}
-		lw := mutil.WrapWriter(w)
+		lw := WrapWriter(w)
 		t1 := time.Now()
 		h.ServeHTTP(lw, r)
 		if lw.Status() == 0 {
 			lw.WriteHeader(http.StatusOK)
 		}
 		t2 := time.Now()
-		printResp(r, lw, t2.Sub(t1))
+		logResponse(r, lw, t2.Sub(t1))
 	}
 	return http.HandlerFunc(fn)
 }
 
-func printResp(r *http.Request, w mutil.WriterProxy, dt time.Duration) {
+func write(b *bytes.Buffer, color string, format string, args ...interface{}) {
+	if colorTTY {
+		fmt.Fprintf(b, color)
+	}
+	fmt.Fprintf(b, format, args...)
+	if colorTTY {
+		fmt.Fprintf(b, ansi.Reset)
+	}
+}
+
+func logResponse(r *http.Request, w WriterProxy, dt time.Duration) {
 	var buf bytes.Buffer
-	cW(&buf, bMagenta, "%s ", r.Method)
+	// write HTTP method
+	write(&buf, ansi.Magenta, "%s ", r.Method)
+	// split URL based on query params
 	urlsplit := strings.Split(r.URL.String(), "?")
 	url := urlsplit[0]
 	cs := strings.Split(url, "/")
+	// write out base URL
 	if len(cs) == 2 && cs[0] == "" && cs[1] == "" {
-		cW(&buf, bBlack, "/")
+		write(&buf, ansi.Black, "/")
 	} else {
 		for _, c := range cs {
 			if c != "" {
-				cW(&buf, bBlack, "/")
-				cW(&buf, bBlue, c)
+				write(&buf, ansi.Black, "/")
+				write(&buf, ansi.Blue, c)
 			}
 		}
 	}
+	// if query params, hash them and write it out
 	if len(urlsplit) > 1 {
 		// hash query params
-		cW(&buf, bBlack, "?")
+		write(&buf, ansi.Black, "?")
 		hash := xxhash.Checksum32([]byte(urlsplit[1]))
-		cW(&buf, bGreen, "%#x ", hash)
+		write(&buf, ansi.Green, "%#x ", hash)
 	} else {
 		buf.WriteString(" ")
 	}
+	// write out response status
 	status := w.Status()
 	if status < 200 {
-		cW(&buf, bBlue, "%03d", status)
+		write(&buf, ansi.Blue, "%03d", status)
 	} else if status < 300 {
-		cW(&buf, bGreen, "%03d", status)
+		write(&buf, ansi.Green, "%03d", status)
 	} else if status < 400 {
-		cW(&buf, bCyan, "%03d", status)
+		write(&buf, ansi.Cyan, "%03d", status)
 	} else if status < 500 {
-		cW(&buf, bYellow, "%03d", status)
+		write(&buf, ansi.Yellow, "%03d", status)
 	} else {
-		cW(&buf, bRed, "%03d", status)
+		write(&buf, ansi.Red, "%03d", status)
 	}
+	// write out duration in milliseconds
 	buf.WriteString(" in ")
 	if dt < 500*time.Millisecond {
-		cW(&buf, nGreen, "%.2fms", dt.Seconds()*1000)
+		write(&buf, ansi.Green, "%.2fms", dt.Seconds()*1000)
 	} else if dt < 5*time.Second {
-		cW(&buf, nYellow, "%.2fms", dt.Seconds()*1000)
+		write(&buf, ansi.Yellow, "%.2fms", dt.Seconds()*1000)
 	} else {
-		cW(&buf, nRed, "%.2fms", dt.Seconds()*1000)
+		write(&buf, ansi.Red, "%.2fms", dt.Seconds()*1000)
 	}
-	//buf.WriteString(" to ")
-	//buf.WriteString(r.RemoteAddr)
+	// log the output according to status code
 	if status < 500 {
 		log.Info(buf.String())
 	} else {
